2433091d0ba81b6efd80d2c664e3f106bb11e9f7
Sundhar Vinodh Sangeetha <sundharvs@gmail.com>
2025-08-28 12:36:04 -0400

    Check if Pc meets grammar when Pc is equivalent and PC is not

--- experiment_1/failure_analysis.py
+++ experiment_1/failure_analysis.py
@@ -1,11 +1,30 @@
 import spot
 import argparse
 import pandas as pd
+import lark
+from prompt_utils import get_llama_bnf_spec
+from gbnf_to_lark import gbnf_to_lark
 
 def parse_args():
     argparser = argparse.ArgumentParser(description="Failure analysis for NL to TL translation")
-    argparser.add_argument("--experiment_csv", type=str, required=True, help="Path to CSV containing experiment trial results")
+    argparser.add_argument("--experiment_csv", type=str, required=False, help="Path to CSV containing experiment trial results", default="trials_250entries_1trials_10examples/trial_1.csv")
     return argparser.parse_args()
+
+def analyze_constrained_loss(exp_df):
+    # This function is meant to analyze the loss in accuracy due to constrained generation
+    gap_entries = exp_df[(exp_df['gemma_Pc_equivalence']) & (~exp_df['gemma_PC_equivalence'])]
+    gap_entries = gap_entries[['nl_sentence', 'propositions', 'dataset_tl', 'gemma_Pc_tl', 'gemma_PC_tl']]
+    print(f"Number of entries where constrained generation failed but unconstrained succeeded: {len(gap_entries)}")
+
+    # Check if the unconstrained translations still meet the EBNF grammar using Lark
+    for index, row in gap_entries.iterrows():
+        grammar = gbnf_to_lark(get_llama_bnf_spec(propositions=row['propositions']))
+        parser = lark.Lark(grammar, start='start', parser='earley')
+        try:
+            parser.parse(row['gemma_Pc_tl'])
+            print(f"Row {index}: Unconstrained translation is valid.\n")
+        except lark.exceptions.LarkError:
+            print(f"Row {index}: Unconstrained translation is INVALID.\nConstrainted TL: {row['gemma_Pc_tl']}\nUnconstrained TL: {row['gemma_PC_tl']}\n")
 
 
 def generate_containment_stats(exp_df):
@@ -38,5 +57,6 @@ if __name__ == "__main__":
 
     exp_df = pd.read_csv(experiment_csv)
 
-    containment_df = generate_containment_stats(exp_df)
-    print(containment_df)
+    analyze_constrained_loss(exp_df)
+    #containment_df = generate_containment_stats(exp_df)
+    #print(containment_df)
